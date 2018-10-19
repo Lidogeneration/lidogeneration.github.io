03bc958fcabcba22920eafe87f20b52b7e067279
Lidogeneration <42538869+Lidogeneration@users.noreply.github.com>
2018-10-19 16:26:44 +0300

    Add files via upload

--- bag/js/main.js
+++ bag/js/main.js
@@ -6,7 +6,32 @@ $('.slider-wrap').slick({
     arrows: false,
     dots: true
 });   
-
+$(document).ready(function() {
+	$('.magncheck').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+});
 AOS.init();
 $('.nav-link-new').mPageScroll2id();
 $('.button-nav, .products-buy-button').magnificPopup();
