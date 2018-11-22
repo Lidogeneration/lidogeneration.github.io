74cb08e4530720ab18ddc2f763afc9a10bbc702f
Rodia26 <43242275+Rodia26@users.noreply.github.com>
2018-11-22 15:39:50 +0200

    Add files via upload

--- avtofishka/js/func.js
+++ avtofishka/js/func.js
@@ -2,6 +2,7 @@ $(function() {
     AOS.init();
     FormCheck();
     $('.a_contact').magnificPopup();
+    SlickReviews();
 });
 
 function FormCheck() {
@@ -11,3 +12,43 @@ function FormCheck() {
         jQuery('.btn_send').prop("disabled", true);
     } }); 
 };
+
+function SlickReviews() {
+    $('.reviews').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  centerMode: true,
+                  centerPadding: '20px',
+                  slidesToShow: 3
+                }
+              },
+            {
+              breakpoint: 768,
+              settings: {
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+              }
+            }
+          ],
+		prevArrow: '<button type="button" class="b-slick-prev b-slick-style"><img src="images/reviews/back.png" class="b_prev"></button>',
+		nextArrow: '<button type="button" class="b-slick-next b-slick-style"><img src="images/reviews/next.png" class="b_prev"></button>'
+    });
+};
+
