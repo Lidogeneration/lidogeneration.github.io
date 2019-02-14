f52ab5cfd25b693725871a4bdf3fea1153ecd57b
Lidogeneration <42538869+Lidogeneration@users.noreply.github.com>
2019-02-14 18:08:48 +0200

    New test

---
+++ test/js/main.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typed', {
+    strings: ['разработка сайтов', 'Создание Landing Page', 'Контекстная реклама','Продвижение в Facebook','Продвижение в Instagram','Интернет-маркетинг'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+  });
