eb0f1269fc7c50fe522a55ff5bc3b78dee4f6a4a
Lidogeneration <42538869+Lidogeneration@users.noreply.github.com>
2019-02-20 16:46:52 +0200

    new art

---
+++ art-ea2/about.php
@@ -0,0 +1,46 @@
+<section class="about parallax-window" data-parallax="scroll" data-image-src="<?php bloginfo(template_url);?>/img/2_bg.jpg" data-z-index="-1">
+<div class="wrap-about" id="about-link"> 
+  <div class="container-fluid" data-aos="zoom-in" data-aos-duration="1000">
+           <h2 class="centred">ЛидоГенерация</h2>
+           <h3 class="text-center">Смотрите видео и узнайте с чего начать!</h3>
+        <hr class="centred">
+    <div class="row align-items-center justify-content-center">
+      <div class="col-md-6">
+        <iframe width="100%" height="315px" src="https://www.youtube.com/embed/oX1DnGTSbNU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      </div>
+      <div class="col-md-6">
+
+        <h4>Лидогенерация - это команда профессионалов своего дела, относящихся к заданиям со здравым фанатизмом, 100% перфекционизмом и чувством юмора, работающие только на результат! Сотрудничество с нами  даст Вам возможность быть на несколько  шагов впереди ваших конкурентов, а также увеличит объемы ваших продаж  в несколько раз. Ваш успех - это индикатор нашей результативности. Продавай с нами!</h4>
+      </div>
+    </div>
+    <div class="centred">
+        <h3>Оставьте заявку и получите коммерческое предложение</h3>
+        <a href="#form-order-price" class="buttom-form-price">Получить...</a>
+    </div>    
+    <!--
+    <div class="row number">
+        <div class="col-md-3 col-6 count1 count">
+            <div class="testimonial-border-md-white"></div>
+            <span class="counter">55</span>
+            <p>Цифры 01</p>
+        </div>
+        <div class="col-md-3 col-6 count2 count">
+            <div class="testimonial-border-md-white"></div>
+            <span class="counter">156</span>
+            <p>Цифры 02</p>
+        </div>
+        <div class="col-md-3 col-6 count3 count">
+            <div class="testimonial-border-md-white"></div>
+            <span class="counter ">160</span>
+            <p>Цифры 03</p>
+        </div>
+        <div class="col-md-3 col-6 count4 count">
+            <div class="testimonial-border-md-white"></div>
+            <span class="counter">99</span>
+            <p>Цифры 04</p>
+        </div>
+    </div>
+-->
+  </div>
+</div>
+</section>
