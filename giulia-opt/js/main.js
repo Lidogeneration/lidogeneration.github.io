3925e194ab268fcaf4f2c0369f788cfcea4d8fae
Lidogeneration <42538869+Lidogeneration@users.noreply.github.com>
2019-04-03 16:58:40 +0300

    Update

--- giulia-opt/js/main.js
+++ giulia-opt/js/main.js
@@ -182,3 +182,50 @@ $('.sliderRew').slick({
 		fixedContentPos: false
 	});
 });
+
+$('.sliderRewN, .sliderRewNSert').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    adaptiveHeight: false,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: true,
+            slidesToShow: 1
+          }
+        }
+    ]
+  });
+
+$(document).ready(function() {
+	$('.sliderRewNSert').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+		
+			}
+		},
+		gallery: {
+            enabled: true,
+            tCounter: '<span class="mfp-counter"></span>'
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+});
