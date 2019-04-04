f93358be265b179c2565f095a29d25759e62c6bc
Lidogeneration <42538869+Lidogeneration@users.noreply.github.com>
2019-04-04 16:25:23 +0300

    update

--- izoliatcia/js/main.js
+++ izoliatcia/js/main.js
@@ -269,3 +269,15 @@ $(document).ready(function() {
 //         videoMain.pause();
 //       }
 // };
+
+$(document).ready(function() {
+	$('.popup-youtube').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
--- izoliatcia/page-done.php
+++ izoliatcia/page-done.php
@@ -2,7 +2,7 @@
 <section class="done d-flex justify-content-center align-items-center">
   <div class="text-center">
   <h1><i class="far fa-check-circle"></i></h1>
-  <h1>Спасибо за завку!</h1>
+  <h1>Спасибо за заявку!</h1>
   <h2>Наши менеджеры свяжутся с Вами в течении 10 минут</h2>
   <h3>Письмо с прайс-листом может попасть в папку СПАМ</h3>
   </div>
