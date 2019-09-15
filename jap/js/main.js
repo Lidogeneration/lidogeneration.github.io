0044eadf632e7395f62168f38357d8c2782042e7
Lidogeneration <42538869+Lidogeneration@users.noreply.github.com>
2019-09-15 17:58:37 +0300

    add

--- jap/js/main.js
+++ jap/js/main.js
@@ -220,3 +220,30 @@ $('.sertCont').slick({
         }
     ]
 });
+
+$(document).ready(function() {
+	$('.sertCont').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+});
