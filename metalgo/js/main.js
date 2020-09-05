052b2cbb81efba72e38d0e4787cf48b592f55d24
alvion4ik <alvion4ik@gmail.com>
2020-09-05 13:32:31 +0300

    тфм вщту

--- metalgo/js/main.js
+++ metalgo/js/main.js
@@ -1,8 +1,37 @@
-const nameScroll = document.getElementById('input_order');
-const customTextFormSpan = document.getElementById('customTextForm');
+'use strict';
 
-function setNameScroll(namescroll, customTextFormSpanF) {
-    nameScroll.value = namescroll;
-    customTextFormSpan.textContent = customTextFormSpanF;
-}
+document.addEventListener('DOMContentLoaded', () => {
+    ///mobileMenu
+    const btnOpen = document.querySelector('#btnOpen'),
+        menuList = document.querySelector('#menuList'),
+        btnClose = document.querySelector('#btnClose'),
+        overlay = document.querySelector('#overlay');
 
+    const closeMenu = () => {
+            menuList.classList.remove('open-menu');
+            overlay.classList.remove('open-menu');
+    };
+    const openMenu = () => {
+        menuList.classList.add('open-menu');
+        overlay.classList.add('open-menu');
+    };
+    btnOpen.addEventListener('click', () => {
+        openMenu();
+    });
+    btnClose.addEventListener('click', () => {
+        closeMenu();
+    });
+    
+    menuList.addEventListener('click', e => {
+        if(menuList.contains(e.target)) {
+            closeMenu();
+        }
+    });
+    document.addEventListener('click', e => {
+        if(menuList.classList.contains('open-menu')) {
+            if(e.target === overlay) {
+                closeMenu();
+            }
+        }
+    });
+});
