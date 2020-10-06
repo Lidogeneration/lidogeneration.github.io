82d978e9c836bfb78fe738027ca5fad380cf021f
alvion4ik <alvion4ik@gmail.com>
2020-10-07 00:26:34 +0300

    mohovik

---
+++ mohovik/js/main.js
@@ -0,0 +1,63 @@
+'use strict';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const nameScroll = document.getElementById('input_order');
+    const customTextFormSpan = document.getElementById('customTextForm');
+    
+    function setNameScroll(namescroll, customTextFormSpanF) {
+        nameScroll.value = namescroll;
+        customTextFormSpan.textContent = customTextFormSpanF;
+    }
+    
+    window.addEventListener('scroll', () => {
+        console.log(document.documentElement.scrollTop);
+        if(document.documentElement.scrollTop > 1) {
+            navBar.classList.add('navbarwhite');
+        }
+        else {
+            navBar.classList.remove('navbarwhite');
+        }
+    });
+
+    if(window.innerWidth <= 992)
+    {
+        AOS.init({
+            disable: true
+        });
+    }
+    
+    
+    
+    const btnToltip = document.querySelectorAll('[data-rol="btnToltip"]');
+    if(btnToltip) {
+        btnToltip.forEach(key => {
+            key.addEventListener('click', e => {
+                btnToltip.forEach(key => {
+                    let tiltipP = key.querySelector('p');
+                    tiltipP.classList.remove('showToltip');
+                });
+                let tiltipP = e.target.querySelector('p');
+                tiltipP.classList.add('showToltip');
+            });
+        });
+    }
+    const navBtn = document.querySelector('[data-rol="navBtn"]'),
+    navList = document.querySelector('[data-rol="navList"]'),
+    navLiHide = document.querySelectorAll('[data-rol="navLiHide"]'),
+    navBar = document.querySelector('[data-rol="navBar"]');
+    if(navBtn && document.documentElement.clientWidth <= 992) {
+        navBtn.addEventListener('click', () => {
+            navBtn.classList.toggle('mobileBtnIn');
+            navList.classList.toggle('showMenuList');
+        });
+        navLiHide.forEach( key => {
+            key.addEventListener('click', () => {
+                navBtn.classList.toggle('mobileBtnIn');
+                navList.classList.toggle('showMenuList');
+            });
+        });
+    }
+
+});
