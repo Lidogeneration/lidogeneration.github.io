1ac4c80691d8f0e8b6a6fa471e321296b5647e3e
alvion4ik <alvion4ik@gmail.com>
2020-10-18 15:00:44 +0300

    fixed first land

--- mohovik/js/plug.js
+++ mohovik/js/plug.js
@@ -31,6 +31,53 @@ $(document).ready(function(){
 
 AOS.init();
 
+
+const forGalleryOne = document.querySelectorAll('#forGalleryOne img');
+let forGalleryOneItems = [];
+forGalleryOne.forEach(key => {
+  forGalleryOneItems.push({src: key.src});
+});
+
+const forGalleryTwo = document.querySelectorAll('#forGalleryTwo img');
+let forGalleryTwoItems = [];
+forGalleryTwo.forEach(key => {
+  forGalleryTwoItems.push({src: key.src});
+});
+
+const forGalleryThree = document.querySelectorAll('#forGalleryThree img');
+let forGalleryThreeItems = [];
+forGalleryThree.forEach(key => {
+  forGalleryThreeItems.push({src: key.src});
+});
+
+console.log(forGalleryOneItems);
+console.log(forGalleryTwoItems);
+console.log(forGalleryThreeItems);
+
+// Example with single object
+$('#galleryOne').magnificPopup({
+  items: forGalleryOneItems,
+  gallery: {
+    enabled: true
+  },
+  type: 'image' // this is default type
+});
+// Example with single object
+$('#galleryTwo').magnificPopup({
+  items: forGalleryTwoItems,
+  gallery: {
+    enabled: true
+  },
+  type: 'image' // this is default t
+});
+// Example with single object
+$('#galleryThree').magnificPopup({
+  items: forGalleryThreeItems,
+  gallery: {
+    enabled: true
+  },
+  type: 'image' // this is default type
+});
 // $(document).ready(function() {
 // 	$('.popup-youtube').magnificPopup({
 // 		type: 'iframe',
