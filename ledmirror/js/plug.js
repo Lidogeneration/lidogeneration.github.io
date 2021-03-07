d1eed782d07891032ef5eb5e6f46916b445bc669
alvion4ik <alvion4ik@gmail.com>
2021-03-07 10:18:41 +0200

    update all files

---
+++ ledmirror/js/plug.js
@@ -0,0 +1,93 @@
+$("a[rel='m_PageScroll2id']").mPageScroll2id({
+  highlightClass: "active"
+});
+
+$('.mpopup').magnificPopup();
+
+
+$('.phoneidcl').inputmask('+38(999) 999-99-99',{ "oncomplete": function(){ 
+  $(".disbutt").prop("disabled", false);
+}, "onincomplete": function(){ $(".disbutt").prop("disabled", true); } });
+
+
+
+$(document).ready(function(){
+  $('.order-main').submit(function(e){
+      e.preventDefault();
+      $.magnificPopup.close(); 
+      $(".disbutt").prop("disabled", true);
+      $.ajax({
+          type:'POST',
+          url:'/wp-content/themes/sad/tele_order.php',
+          data: $(this).serialize()
+      }).done(function(){
+              $.magnificPopup.close(); 
+          setTimeout(function(){
+              window.location = 'done';
+          },1);
+      });
+      });
+});
+
+
+// $(document).ready(function() {
+// 	$('.popup-youtube').magnificPopup({
+// 		type: 'iframe',
+// 		mainClass: 'mfp-fade',
+// 		removalDelay: 160,
+// 		preloader: false,
+// 		fixedContentPos: false
+// 	});
+// });
+
+// $('.revolution__slider').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 7000,
+//   speed: 1000,
+//   dots: true,
+//   arrows: true,
+//   pauseOnFocus: false,
+//   pauseOnHover: false,
+//   infinite: true,
+//   accessibility: false,
+//   prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-caret-left"></i></button>',
+//   nextArrow: '<button type="button" class="slick-next"><i class="fas fa-caret-right"></i></button>',
+// });
+
+$('.image-popup-no-margins').magnificPopup({
+  type: 'image',
+  closeOnContentClick: true,
+  closeBtnInside: false,
+  fixedContentPos: true,
+  mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+  image: {
+    verticalFit: true
+  },
+  zoom: {
+    enabled: true,
+    duration: 300 // don't foget to change the duration also in CSS
+  }
+});
+
+$(document).ready(function() {
+	$('.about__cont-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+});
