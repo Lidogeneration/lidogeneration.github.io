d90695facac5210c4ae72b6c66e0f74061ab77b8
alvion4ik <alvion4ik@gmail.com>
2021-03-21 12:36:21 +0200

    first scroll

--- ledmirror/js/plug.js
+++ ledmirror/js/plug.js
@@ -91,3 +91,57 @@ $(document).ready(function() {
 		
 	});
 });
+
+
+const timerDate = document.getElementById('timerId');
+
+if(timerDate){
+
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes
+        };
+    }
+
+    function initializeClock(id, endtime) {
+
+        var clock = document.getElementById(id);
+        var daysSpan = clock.querySelector('#days');
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+
+        function updateClock() {
+        var t = getTimeRemaining(endtime);
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        //   secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+        // if (t.total <= 0) {
+
+        //   clearInterval(timeinterval);
+        //   var deadline = new Date(Date.parse(new Date()) + 260000 * 1000);
+        //   initializeClock('clockdivS', deadline);
+        //   initializeClock('clockdiv', deadline);
+        // }
+        }
+        
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+    }
+    var tl = new Date(timerDate.textContent);
+    var deadline = new Date(tl);
+    var tOld = getTimeRemaining(deadline);
+    if (tOld.total <=0)
+    {
+    deadline = new Date(Date.parse(new Date()) + 250000 * 1000);
+    }
+    initializeClock('clockdivS', deadline);
+    //   initializeClock('clockdiv', deadline);
+}
